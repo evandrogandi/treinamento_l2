36dd2e60db3fcb998d53896b454e58510ad1a6dc
Evandro Gandelini <evandrogandelini@atec.com.br>
2017-12-07 20:31:06 -0200

    first commit

---
+++ Treinamento/Aula02Poo/Model/Local.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Aula02Poo.Model
+{
+    class Local
+    {
+        public int Size { get; set; } = 10;
+
+        public string Code { get; set; }
+
+
+        public Local()
+        {
+
+        }
+
+        public Local(string code, int size)
+        {
+            Code = code;
+            Size = size;
+        }
+
+        public virtual void Harvest()
+        {
+
+
+        }
+
+    }
+
+    class Sector : Local
+    {
+        
+        public Farm[] Farms { get; set; }
+
+        public Sector(string code, int size)
+            : base(code, size)
+        {
+        }
+
+        public override void Harvest()
+        {
+            Console.WriteLine("Harvest Sector {0}", Code);
+        }
+    }
+
+    class Farm : Local
+    {
+        public Block[] Blocks { get; set; }
+
+        public override void Harvest()
+        {
+            Console.WriteLine("Harvest Blocks {0}", Code);
+        }
+
+    }
+
+    class Block : Local
+    {
+
+        public Field[] Fields { get; set; }
+
+        public override void Harvest()
+        {
+            Console.WriteLine("Harvest Block {0}", Code);
+        }
+
+    }
+
+    class Field : Local
+    {
+        private string _culture;
+
+        public string Culture
+        {
+            get
+            {
+                return _culture;
+            }
+
+            set
+            {
+                Culture = _culture;
+            }
+        }
+        public void Plant(string culture)
+        {
+            Culture = culture;
+        }
+
+        public Field(string code)
+        {
+            Code = code;
+        }
+
+        public override void Harvest()
+        {
+            Console.WriteLine("Harvest Field {0} Culture {1}", Code, Culture);
+        }
+
+    }
+}
---
+++ Treinamento/Aula02Poo/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Aula02Poo.Model;
+
+namespace Aula02Poo
+{
+    class Program
+    {
+
+        public static void Print(object o)
+        {
+            Console.WriteLine(o.ToString());
+
+        }
+
+        public static void Show(Local local)
+        {
+            Console.WriteLine("{0} - {1}", local.Code, local.Size);
+        }
+
+        static void Main(string[] args)
+        {
+            int i = 10;
+            bool b = true;
+
+            string s = "Evandro";
+
+
+            Print(i);
+            Print(s);
+            Print(b);
+
+            Sector l1 = new Sector("S1", 100);
+            Farm f1 = new Farm();
+            f1.Code = "F1";
+
+            Field f2 = new Field("Field2");
+            f2.Culture = "Soja";
+
+            Show(l1);
+
+            Show(f2);
+
+            Console.ReadKey();
+        }
+    }
+}
